af7e3fd35aa5aa9abacd4c09171507e2b894cb1a
JayXon <jayxon@gmail.com>
2014-08-19 15:05:39 -0400

    PE: overlap PE Header and DOS Header to save more bytes

--- formats/pe.cpp
+++ formats/pe.cpp
@@ -7,9 +7,9 @@ const unsigned char Pe::header_magic[] = { 'M', 'Z' };
 size_t Pe::Leanify(size_t size_leanified /*= 0*/)
 {
 
-    uint32_t pe_header = *(uint32_t *)(fp + 0x3C);
+    uint32_t pe_header_offset = *(uint32_t *)(fp + 0x3C);
     // check PE signature: PE00
-    if (pe_header < size && *(uint32_t *)(fp + pe_header) != 0x00004550)
+    if (pe_header_offset < size && *(uint32_t *)(fp + pe_header_offset) != 0x00004550)
     {
         std::cout << "Not a PE file." << std::endl;
         if (size_leanified)
@@ -19,118 +19,161 @@ size_t Pe::Leanify(size_t size_leanified /*= 0*/)
         }
         return size;
     }
-    ImageFileHeader *pimage_file_header = (ImageFileHeader *)(fp + pe_header + 4);
 
-    // 0x0001: IMAGE_FILE_RELOCS_STRIPPED
+    ImageFileHeader *image_file_header = (ImageFileHeader *)(fp + pe_header_offset + 4);
+    size_t total_header_size = 0x28 + image_file_header->SizeOfOptionalHeader + sizeof(ImageSectionHeader) * image_file_header->NumberOfSections;
+
+    const uint32_t new_pe_header_offset = 0x10;
+
+    // if original file's PE header is not overlapped with DOS header
+    // then we can overlap it to save some space
+    if (pe_header_offset >= 0x40)
+    {
+        memcpy(fp - size_leanified, header_magic, sizeof(header_magic));
+        memset(fp - size_leanified + sizeof(header_magic), 0, new_pe_header_offset - sizeof(header_magic));
+        memmove(fp - size_leanified + new_pe_header_offset, fp + pe_header_offset, total_header_size - new_pe_header_offset);
+        image_file_header = (ImageFileHeader *)(fp - size_leanified + 0x14);
+        // set new PE Header offset
+        *(uint32_t *)(fp - size_leanified + 0x3C) = new_pe_header_offset;
+    }
+    else
+    {
+        // this file probably already packed with some extreme packer
+        total_header_size += pe_header_offset - new_pe_header_offset;
+        if (size_leanified)
+        {
+            // move entire SizeOfHeaders to make sure nothing was missed
+            memmove(fp - size_leanified, fp, *(uint32_t *)(fp + pe_header_offset + 0x54));
+            image_file_header = (ImageFileHeader *)(fp - size_leanified + pe_header_offset + 4);
+        }
+    }
+
+    ImageOptionalHeader *optional_header = (ImageOptionalHeader *)((char *)image_file_header + sizeof(ImageFileHeader));
+
+    // Data Directories
+    // PE32:    Magic number: 0x10B     Offset: 0x60
+    // PE32+:   Magic number: 0x20B     Offset: 0x70
+    ImageDataDirectory *data_directories = (ImageDataDirectory *)((char *)optional_header + (optional_header->Magic == 0x10B ? 0x60 : 0x70));
+
+    // Section Table
+    ImageSectionHeader *section_table = (ImageSectionHeader *)((char *)optional_header + image_file_header->SizeOfOptionalHeader);
+
+
+
+    // Base Relocation Table VirtualAddress
+    uint32_t reloc_virtual_address = data_directories[5].VirtualAddress;
+
+    uint32_t reloc_raw_offset = 0;
+    uint32_t reloc_raw_size = 0;
+
     // 0x2000: IMAGE_FILE_DLL
-    // only remove reloc section when both bit are not set
-    if (!(pimage_file_header->Characteristics & 0x0001) && !(pimage_file_header->Characteristics & 0x2000))
+    // only remove reloc section if it's not dll
+    if (!(image_file_header->Characteristics & 0x2000))
     {
         // set IMAGE_FILE_RELOCS_STRIPPED
-        pimage_file_header->Characteristics |= 0x0001;
+        image_file_header->Characteristics |= 0x0001;
 
-        char *optional_header = (char *)pimage_file_header + sizeof(ImageFileHeader);
-        uint32_t section_alignment = *(uint32_t *)(optional_header + 0x20);
-
-        // Data Directories
-        // PE32:    Magic number: 0x10B     Offset: 0x60
-        // PE32+:   Magic number: 0x20B     Offset: 0x70
-        ImageDataDirectory *data_directories = (ImageDataDirectory *)(optional_header + (*(uint16_t *)optional_header == 0x10B ? 0x60 : 0x70));
-
-        // Base Relocation Table VirtualAddress
-        uint32_t reloc_virtual_address = data_directories[5].VirtualAddress;
-        // test if it is a valid address
-        // some PE will use this space for other purpose
-        if (reloc_virtual_address % section_alignment == 0 && data_directories[5].Size < size)
-        {
-            data_directories[5].VirtualAddress = 0;
-            // Base Relocation Table Size
-            data_directories[5].Size = 0;
-        }
-
-        // Section Table
-        ImageSectionHeader *section_table = (ImageSectionHeader *)(optional_header + pimage_file_header->SizeOfOptionalHeader);
-        uint32_t reloc_raw_offset = 0;
-        uint32_t reloc_raw_size = 0;
-        for (int i = 0; i < pimage_file_header->NumberOfSections; i++)
+        for (int i = 0; i < image_file_header->NumberOfSections; i++)
         {
             if (section_table[i].VirtualAddress == reloc_virtual_address)
             {
                 reloc_raw_offset = section_table[i].PointerToRawData;
                 reloc_raw_size = section_table[i].SizeOfRawData;
 
-                pimage_file_header->NumberOfSections--;
+                // remove Relocation Table in Data Directories
+                data_directories[5].VirtualAddress = 0;
+                data_directories[5].Size = 0;
+
+                image_file_header->NumberOfSections--;
 
                 // move sections after reloc forward
-                memmove(&section_table[i], &section_table[i + 1], sizeof(ImageSectionHeader) * (pimage_file_header->NumberOfSections - i));
+                memmove(&section_table[i], &section_table[i + 1], sizeof(ImageSectionHeader) * (image_file_header->NumberOfSections - i));
 
-                // fill the last section with 0
-                memset(&section_table[pimage_file_header->NumberOfSections], 0, sizeof(ImageSectionHeader));
+                total_header_size -= sizeof(ImageSectionHeader);
                 break;
             }
         }
-        if (reloc_raw_size)
+    }
+
+    uint32_t header_size_aligned = ((total_header_size - 1) & ~(optional_header->FileAlignment - 1)) + optional_header->FileAlignment;
+    // has to be multiple of FileAlignment
+    size_t pe_size_leanified = 0;
+    if (header_size_aligned < optional_header->SizeOfHeaders)
+    {
+        pe_size_leanified = optional_header->SizeOfHeaders - header_size_aligned;
+        optional_header->SizeOfHeaders = header_size_aligned;
+    }
+    
+    // fill the rest of the headers with 0
+    // only if PE Header offset has changed
+    if (pe_header_offset >= 0x40)
+    {
+        pe_header_offset = new_pe_header_offset;
+        memset(fp - size_leanified + total_header_size, 0, header_size_aligned - total_header_size);
+    }
+
+
+    if (reloc_raw_size)
+    {
+        // multiple of SectionAlignment
+        uint32_t reloc_virtual_size = ((reloc_raw_size - 1) & ~(optional_header->SectionAlignment - 1)) + optional_header->SectionAlignment;
+
+        // decrease SizeOfImage
+        optional_header->SizeOfImage -= reloc_virtual_size;
+
+        // set ASLR in DllCharacteristics to false
+        // it seems this isn't necessary
+        // *(uint16_t *)(optional_header + 0x46) &= ~0x0040;
+
+
+        // move other section address forward if it is behind reloc
+        for (int i = 0; i < image_file_header->NumberOfSections; i++)
         {
-            // multiple of SectionAlignment
-            uint32_t reloc_virtual_size = ((reloc_raw_size - 1) & ~(section_alignment - 1)) + section_alignment;
-
-            // decrease SizeOfImage
-            *(uint32_t *)(optional_header + 0x38) -= reloc_virtual_size;
-
-            // set ASLR in DllCharacteristics to false
-            // it seems this isn't necessary
-            // *(uint16_t *)(optional_header + 0x46) &= ~0x0040;
-
-
-
-            // move other section address forward if it is behind reloc
-            for (int i = 0; i < pimage_file_header->NumberOfSections; i++)
+            if (section_table[i].VirtualAddress > reloc_virtual_address)
             {
-                if (section_table[i].VirtualAddress > reloc_virtual_address)
+                // move rsrc
+                if (section_table[i].VirtualAddress == data_directories[2].VirtualAddress)
                 {
-                    // move rsrc
-                    if (section_table[i].VirtualAddress == data_directories[2].VirtualAddress)
-                    {
-                        MoveRSRC(fp + section_table[i].PointerToRawData, (ImageResourceDirectory *)(fp + section_table[i].PointerToRawData), reloc_virtual_size);
-                    }
-                    section_table[i].VirtualAddress -= reloc_virtual_size;
+                    MoveRSRC(fp + section_table[i].PointerToRawData, (ImageResourceDirectory *)(fp + section_table[i].PointerToRawData), reloc_virtual_size);
                 }
-                if (section_table[i].PointerToRawData > reloc_raw_offset)
-                {
-                    section_table[i].PointerToRawData -= reloc_raw_size;
-                }
+                section_table[i].VirtualAddress -= reloc_virtual_size;
             }
-            // do it with Data Directories too
-            for (int i = 0; i < 16; i++)
+            if (section_table[i].PointerToRawData > reloc_raw_offset)
             {
-                if (data_directories[i].VirtualAddress > reloc_virtual_address)
-                {
-                    data_directories[i].VirtualAddress -= reloc_virtual_size;
-                }
+                section_table[i].PointerToRawData -= reloc_raw_size;
             }
+            section_table[i].PointerToRawData -= pe_size_leanified;
         }
 
+        reloc_raw_offset -= pe_size_leanified;
+
+        // do it with Data Directories too
+        for (int i = 0; i < 16; i++)
+        {
+            if (data_directories[i].VirtualAddress > reloc_virtual_address)
+            {
+                data_directories[i].VirtualAddress -= reloc_virtual_size;
+            }
+        }
+    }
+
+    fp -= size_leanified;
+    size -= pe_size_leanified;
+    if (reloc_raw_size)
+    {
         // move everything before reloc
-        if (size_leanified)
-        {
-            fp -= size_leanified;
-            memmove(fp, fp + size_leanified, reloc_raw_offset);
-        }
+        memmove(fp + header_size_aligned, fp + size_leanified + header_size_aligned + pe_size_leanified, reloc_raw_offset - header_size_aligned);
+
         // move everything after reloc
         size -= reloc_raw_size;
         memmove(fp + reloc_raw_offset, fp + size_leanified + reloc_raw_offset + reloc_raw_size, size - reloc_raw_offset);
     }
     else
     {
-        if (size_leanified)
-        {
-            fp -= size_leanified;
-            memmove(fp, fp + size_leanified, size);
-        }
-    }
+        memmove(fp + header_size_aligned, fp + size_leanified + header_size_aligned + pe_size_leanified, size - header_size_aligned);
+    }
 
     return size;
-
 }
 
 
--- formats/pe.h
+++ formats/pe.h
@@ -39,6 +39,39 @@ private:
         uint16_t    Characteristics;
     };
 
+    struct ImageOptionalHeader {
+        uint16_t    Magic;
+        uint8_t     MajorLinkerVersion;
+        uint8_t     MinorLinkerVersion;
+        uint32_t    SizeOfCode;
+        uint32_t    SizeOfInitializedData;
+        uint32_t    SizeOfUninitializedData;
+        uint32_t    AddressOfEntryPoint;
+        uint32_t    BaseOfCode;
+        uint32_t    BaseOfData;     // PE32+ does not have BaseOfData
+        uint32_t    ImageBase;      // PE32+ has uint64_t ImageBase
+        uint32_t    SectionAlignment;
+        uint32_t    FileAlignment;
+        uint16_t    MajorOperatingSystemVersion;
+        uint16_t    MinorOperatingSystemVersion;
+        uint16_t    MajorImageVersion;
+        uint16_t    MinorImageVersion;
+        uint16_t    MajorSubsystemVersion;
+        uint16_t    MinorSubsystemVersion;
+        uint32_t    Win32VersionValue;
+        uint32_t    SizeOfImage;
+        uint32_t    SizeOfHeaders;
+        uint32_t    CheckSum;
+        uint16_t    Subsystem;
+        uint16_t    DllCharacteristics;
+        uint32_t    SizeOfStackReserve;     // do not use anything below if it is PE32+
+        uint32_t    SizeOfStackCommit;
+        uint32_t    SizeOfHeapReserve;
+        uint32_t    SizeOfHeapCommit;
+        uint32_t    LoaderFlags;
+        uint32_t    NumberOfRvaAndSizes;
+    };
+
     struct ImageDataDirectory {
         uint32_t    VirtualAddress;
         uint32_t    Size;
