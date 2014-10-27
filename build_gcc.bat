1e6d56f859d0817ec3f0dcca59ddceb3fd20bec8
JayXon <jayxon@gmail.com>
2014-10-27 00:54:58 -0400

    Win: add version information when build with gcc

--- build_gcc.bat
+++ build_gcc.bat
@@ -1,8 +1,10 @@
 @Echo off
 Pushd %~dp0
 SetLocal EnableDelayedExpansion
-Set Args=--std=c++0x -Wno-attributes -O3 -msse2 -mfpmath=sse -flto -s -o "Leanify" 
+Set Args=--std=c++0x -Wno-attributes -O3 -msse2 -mfpmath=sse -flto -s -o "Leanify" Leanify.res 
 For /r "%~dp0" %%i In (*.cpp *.c *.cc) Do (Set t=%%i && Set Args=!Args!!t:%~dp0=!)
 For %%i In (formats\mozjpeg\jdcolext.c formats\mozjpeg\jdmrgext.c) Do Set Args=!Args:%%i =!
+windres --output-format=coff Leanify.rc Leanify.res
 g++ %Args%
+Del Leanify.res
 Pause
