dcfb0d32700bf274b69c880c63912ccff2b50e42
JayXon <jayxon@gmail.com>
2015-01-05 03:37:53 -0500

    mozjpeg: fix warning about inline in gcc 4.8+

--- build_gcc.bat
+++ build_gcc.bat
@@ -1,7 +1,7 @@
 @Echo off
 Pushd %~dp0
 SetLocal EnableDelayedExpansion
-Set Args=--std=c++0x -Wno-attributes -O3 -msse2 -mfpmath=sse -flto -s -o "Leanify" Leanify.res 
+Set Args=--std=c++0x -O3 -msse2 -mfpmath=sse -flto -s -o "Leanify" Leanify.res 
 For /r "%~dp0" %%i In (*.cpp *.c *.cc) Do (Set t=%%i && Set Args=!Args!!t:%~dp0=!)
 For %%i In (formats\mozjpeg\jdcol565.c formats\mozjpeg\jdcolext.c formats\mozjpeg\jdmrg565.c formats\mozjpeg\jdmrgext.c formats\mozjpeg\jstdhuff.c) Do Set Args=!Args:%%i =!
 windres --output-format=coff Leanify.rc Leanify.res
--- formats/mozjpeg/jconfigint.h
+++ formats/mozjpeg/jconfigint.h
@@ -1,6 +1,6 @@
 #ifndef INLINE
 #if defined(__GNUC__)
-#define INLINE __attribute__((always_inline))
+#define INLINE inline __attribute__((always_inline))
 #elif defined(_MSC_VER)
 #define INLINE __forceinline
 #else
