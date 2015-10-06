c9e4d0f68f60a7112fb6e3eb205053b2dbfa0593
JayXon <jayxon@gmail.com>
2015-10-05 23:42:25 -0700

    DataURI: more detailed log

--- formats/data_uri.cpp
+++ formats/data_uri.cpp
@@ -45,7 +45,7 @@ size_t DataURI::Leanify(size_t size_leanified /*= 0*/)
         {
             if (is_verbose)
             {
-                std::cout << std::string(new_p_read, start - new_p_read) << " found" << std::endl;
+                std::cout << std::string(new_p_read, start + 8 - new_p_read) << "... found at offset 0x" << std::hex << new_p_read - fp << std::endl;
             }
             size_t new_size = Base64(p_read, end - p_read).Leanify(p_read - p_write);
             p_write += new_size;
