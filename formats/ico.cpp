119b72321cd745e01208d1c8ceb08036d5edb8b1
JayXon <jayxon@gmail.com>
2016-03-06 17:51:17 -0800

    ICO: sort the entries by offset
    This enables us to handle rare cases like the entries was not sorted (of
    course), images overlapping with each other, and remove gaps between
    images if any.

--- formats/ico.cpp
+++ formats/ico.cpp
@@ -1,9 +1,13 @@
 #include "ico.h"
 
-#include <cstdint>
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
 #include "png.h"
 
+using std::cerr;
+using std::endl;
 
 const uint8_t Ico::header_magic[] = { 0x00, 0x00, 0x01, 0x00 };
 
@@ -30,53 +34,67 @@ size_t Ico::Leanify(size_t size_leanified /*= 0*/)
     // number of images inside ico file
     uint16_t num_of_img = *(uint16_t *)(fp_ + 4);
 
-    IconDirEntry *icon_dir_entry = reinterpret_cast<IconDirEntry *>(fp_ - size_leanified + 6);
-
-    // invalid Icon file
-    if (6 + num_of_img * 16U >= size_ ||
-        icon_dir_entry->dwImageOffset + icon_dir_entry->dwBytesInRes > size_ ||
-        icon_dir_entry[num_of_img - 1].dwImageOffset + icon_dir_entry[num_of_img - 1].dwBytesInRes > size_)
+    // size too small
+    if (6 + num_of_img * sizeof(IconDirEntry) >= size_)
     {
         return Format::Leanify(size_leanified);
     }
 
-    // move header
-    if (size_leanified)
+    // sort the entries by offset just in case it's not sorted before
+    IconDirEntry *entry_addr = reinterpret_cast<IconDirEntry *>(fp_ + 6);
+    std::vector<IconDirEntry> entries(entry_addr, entry_addr + num_of_img);
+    std::sort(entries.begin(), entries.end(), [] (const IconDirEntry& a, const IconDirEntry& b)
     {
-        memmove(fp_ - size_leanified, fp_, 6 + num_of_img * 16);
+        return a.dwImageOffset < b.dwImageOffset;
+    });
+
+    // check overlaps
+    for (size_t i = 1; i < entries.size(); i++)
+    {
+        if (entries[i - 1].dwImageOffset + entries[i - 1].dwBytesInRes > entries[i].dwImageOffset)
+        {
+            cerr << "Error: Found overlapping icon entries!" << endl;
+            return Format::Leanify(size_leanified);
+        }
     }
 
-    size_t new_size_leanified = 0;
-    while (num_of_img--)
+    // is file size enough?
+    if (entries.back().dwImageOffset + entries.back().dwBytesInRes > size_)
     {
-        uint32_t old_offset = icon_dir_entry->dwImageOffset;
+        return Format::Leanify(size_leanified);
+    }
+
+    for (size_t i = 0; i < entries.size(); i++)
+    {
+        uint32_t old_offset = entries[i].dwImageOffset;
         // write new offset
-        if (new_size_leanified)
+        if (i != 0)
         {
-            icon_dir_entry->dwImageOffset -= new_size_leanified;
+            entries[i].dwImageOffset = entries[i - 1].dwImageOffset + entries[i - 1].dwBytesInRes;
+        }
+        else
+        {
+            entries[i].dwImageOffset = 6 + num_of_img * sizeof(IconDirEntry);
         }
 
         // only Leanify PNG
         if (memcmp(fp_ + old_offset, Png::header_magic, sizeof(Png::header_magic)) == 0)
         {
-            uint32_t new_size = Png(fp_ + old_offset, icon_dir_entry->dwBytesInRes).Leanify(size_leanified + new_size_leanified);
-            if (new_size != icon_dir_entry->dwBytesInRes)
-            {
-                new_size_leanified += icon_dir_entry->dwBytesInRes - new_size;
-                icon_dir_entry->dwBytesInRes = new_size;
-            }
+            entries[i].dwBytesInRes = Png(fp_ + old_offset, entries[i].dwBytesInRes).Leanify(size_leanified + old_offset - entries[i].dwImageOffset);
         }
-        else if (size_leanified + new_size_leanified)
+        else
         {
-            memmove(fp_ + icon_dir_entry->dwImageOffset - size_leanified, fp_ + old_offset, icon_dir_entry->dwBytesInRes);
+            memmove(fp_ + entries[i].dwImageOffset - size_leanified, fp_ + old_offset, entries[i].dwBytesInRes);
         }
-        icon_dir_entry++;
     }
 
     fp_ -= size_leanified;
-    // offset + size of last file
-    icon_dir_entry--;
-    size_ = icon_dir_entry->dwImageOffset + icon_dir_entry->dwBytesInRes;
+
+    // write new entries
+    memcpy(fp_ + 6, entries.data(), entries.size() * sizeof(IconDirEntry));
+
+    // offset + size of last img
+    size_ = entries.back().dwImageOffset + entries.back().dwBytesInRes;
     return size_;
 }
 
